9614873b81bafba3820e5717186e7ba9340d986a
jilvins <59861669+jilvins@users.noreply.github.com>
2021-03-18 13:55:19 +0200

    Some styling and touch functionality added

--- Carousel.js
+++ Carousel.js
@@ -4,33 +4,23 @@ import CarouselItem from './CarouselItem.js'
 
 export default function Carousel () {
 
-    const {slideItems, currentPic, setCurrentPic, setScrollDirection} = useContext(Context)
+    const {slideItems, currentPic, setCurrentPic} = useContext(Context)
 
     function choosePic (e) {
         setCurrentPic(e.target.innerHTML)
     }
 
-    function nextPic () {
-        currentPic <= slideItems.length-1 ? setCurrentPic(currentPic + 1) : setCurrentPic(1)
-        setScrollDirection("toRight")
-      }
-    function prevPic () {
-        currentPic == 1 ? setCurrentPic(slideItems.length) : setCurrentPic(currentPic - 1)
-        setScrollDirection("toLeft")
-      }
-    
-
+ 
     const newSlide = slideItems.filter((slide)=>slide.id==currentPic).map((slide) => (
          
-        <CarouselItem key={slide.id} slide={slide} onTouchMove={nextPic}/>      
+        <CarouselItem key={slide.id} slide={slide}/>      
     )   
     )
 
     return (
         <div className="carousel-component">
             {newSlide}
-            <button onClick={prevPic} className="nav-btn right">{"<"}</button>
-           <button onClick={nextPic} className="nav-btn left">{">"}</button>
+            
         <div>
             {slideItems.map((slide) => (
                 <button key={slide.id} onClick={choosePic} className={`selected-img
@@ -38,7 +28,7 @@ export default function Carousel () {
                 
                 >{slide.id}</button>
             ))}
-            <button onTouchMove={nextPic} className="movable">touch me</button>
+            
         </div>
         </div>
     )
--- CarouselItem.js
+++ CarouselItem.js
@@ -3,13 +3,24 @@ import {Context} from './AppContext.js'
 
 
 export default function CarouselItem ({slide}) {
-    let {scrollDirections} = useContext(Context)
+    let {slideItems, currentPic, setCurrentPic, setScrollDirection, scrollDirections} = useContext(Context)
+
+    function nextPic () {
+        currentPic <= slideItems.length-1 ? setCurrentPic(currentPic + 1) : setCurrentPic(1)
+        setScrollDirection("toRight")
+      }
+    function prevPic () {
+        currentPic == 1 ? setCurrentPic(slideItems.length) : setCurrentPic(currentPic - 1)
+        setScrollDirection("toLeft")
+      }
 
     return (
-        <div className={`carousel-item ${scrollDirections}`}>
-           <img src={slide.imgSrc}/>
-           <button className="right-side"></button>
-           <button className="left-side"></button>
+        <div className="carousel-item">
+           <img src={slide.imgSrc} className={`${scrollDirections}`}/>
+           <button className="right-side" onTouchMove={prevPic}></button>
+           <button className="left-side" onTouchMove={nextPic}></button>
+           <button onClick={prevPic} className="nav-btn right">{"<"}</button>
+           <button onClick={nextPic} className="nav-btn left">{">"}</button>
         </div>
     )
 }
